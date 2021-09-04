eb6404548088e50dae7ea5fc28e0ee20867bc01e
@Sukhbold <col.de.staz@gmail.com>
2021-09-05 02:22:12 +0800

    .

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,3 @@
-import jwt from 'jsonwebtoken'
-
 export async function getUser(ctx) {
-   const token = jwt.sign({ user: 'admin' }, process.env.ACCESS_TOKEN_SECRET)
-   ctx.body = { token: token }
+   ctx.body = {user: 'hello'}
 }
