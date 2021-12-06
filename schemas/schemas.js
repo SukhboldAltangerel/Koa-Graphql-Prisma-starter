3ef8fe1ee526834e29dec526a1c01b5722c58e85
Sukhbold Altangerel <col.de.staz@gmail.com>
2021-12-06 18:41:46 +0800

    .

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,22 +1,63 @@
-import { statSync, readdirSync, lstatSync } from 'fs'
-import path from 'path'
+import { statSync, readdirSync, lstatSync, readdir } from 'fs'
+import path, { join, dirname } from 'path'
 
 // const read = readdirSync('schemas/test')
 
 // console.log(read)
 
-const getFiles = path => {
-   const files = []
-   for (const file of readdirSync(path)) {
-      const fullPath = path + '/' + file
-      if (lstatSync(fullPath).isDirectory())
-         getFiles(fullPath).forEach(x => files.push(file + '/' + x))
-      else files.push(file)
+// const getFiles = path => {
+//    const files = []
+//    for (const file of readdirSync(path)) {
+//       const fullPath = path + '/' + file
+//       if (lstatSync(fullPath).isDirectory())
+//          getFiles(fullPath).forEach(x => files.push(file + '/' + x))
+//       else files.push(file)
+//    }
+//    return files
+// }
+
+// console.log(getFiles('schemas/test'))
+
+// const __dirname = path.resolve()
+
+// const pathSchema = join(__dirname, 'schemas/test')
+
+// console.log(import('./test/main.js'))
+
+// function getImports(path) {
+//    const imports = []
+//    for (const file of readdirSync(path)) {
+//       const pathFull = join(path, file)
+//       if (lstatSync(pathFull).isDirectory()) {
+//          getImports(pathFull).forEach(file => imports.push(import(join(pathFull, file))))
+//       } else {
+//          // imports.push(import())
+//       }
+//    }
+//    return imports
+// }
+
+// console.log(getImports(pathSchema))
+
+async function getImports(path = 'schemas/test') {
+   for (const file of await readdir(path)) {
+      console.log('🚀', join(__dirname, file))
+      // yield await import(join(__dirname, file))
    }
-   return files
 }
 
-console.log(getFiles('schemas/test'))
+getImports()
+
+const imports = {}
+let i = 0;
+
+// (async () => {
+//    for await (const im of getImports()) {
+//       imports[i++] = im
+//    }
+//    console.log(imports)
+// })()
+
 
 
 // import { promises } from 'fs'
